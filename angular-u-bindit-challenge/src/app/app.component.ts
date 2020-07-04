a014ef2d5039699c2bf12cd3d6be058e8b92ebe7
Muhammad <faraz.edu@hotmail.com>
2020-07-04 22:25:03 +0500

    test push

--- angular-u-bindit-challenge/src/app/app.component.ts
+++ angular-u-bindit-challenge/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bind-it';
   myNumbers: number[] = [];
-  oddNumbers: number[] = [];
+  oddNumbers: number[] = []; 
 
   catchIntervalValue(myNumber: number)
   {
